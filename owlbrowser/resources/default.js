431c733666e76beca2bc9a09c5f723ff895decbf
NickIsConfused <NickIsConfused@3bc1f533-9450-0410-815e-09e6cc51effc>
2010-07-29 22:44:23 +0000

    bugfix: setting options when session in url

--- owlbrowser/resources/default.js
+++ owlbrowser/resources/default.js
@@ -61,7 +61,11 @@ function reloadAllFrames(){
         }
     }
     else{
-        location.replace(location);
+        // we have to strip out the session if it is in the url
+        // otherwise the previous state will just be reloaded
+        var url = location.toString();
+        url = url.replace(/[&|?]session=[^&]+/, "");
+        location.replace(url);
     }
 }
 
