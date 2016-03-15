00bde93f06300885e6cc9d1de1d0ae228cf9f2e9
ignazio <ignazio.palmisano@gmail.com>
2016-03-15 23:54:33 +0000

    Fix Bug in domain extraction in html rendering #64
    Doclet assumed that any request for domains is for data properties.
    Instead, check for data.object/object expressions. If all fail, the 
    user object can only be an annotation property. The doclet cannot
    represent IRI domains, only class expressions domains, so return empty
    here.

--- owlhtml/src/main/java/org/coode/html/doclet/DomainsDoclet.java
+++ owlhtml/src/main/java/org/coode/html/doclet/DomainsDoclet.java
@@ -4,21 +4,25 @@
 package org.coode.html.doclet;
 
 import java.util.Collection;
+import java.util.Collections;
 import java.util.Set;
 
 import org.coode.html.OWLHTMLKit;
 import org.semanticweb.owlapi.model.OWLClassExpression;
+import org.semanticweb.owlapi.model.OWLObjectPropertyExpression;
 import org.semanticweb.owlapi.model.OWLOntology;
 import org.semanticweb.owlapi.model.OWLProperty;
 import org.semanticweb.owlapi.search.EntitySearcher;
 
 /**
  * Author: Nick Drummond<br>
- * http://www.cs.man.ac.uk/~drummond/<br><br>
+ * http://www.cs.man.ac.uk/~drummond/<br>
+ * <br>
  * <p/>
  * The University Of Manchester<br>
  * Bio Health Informatics Group<br>
- * Date: Jan 25, 2008<br><br>
+ * Date: Jan 25, 2008<br>
+ * <br>
  */
 public class DomainsDoclet<O extends OWLProperty> extends AbstractOWLElementsDoclet<O, OWLClassExpression> {
 
@@ -29,7 +33,15 @@ public class DomainsDoclet<O extends OWLProperty> extends AbstractOWLElementsDoc
     @Override
     protected Collection<OWLClassExpression> getAssertedElements(Set<OWLOntology> onts) {
         O userObject = getUserObject();
-        return EntitySearcher.getDomains(userObject.asOWLDataProperty(),
-                onts);
+        if (userObject.isOWLDataProperty()) {
+            return EntitySearcher.getDomains(userObject.asOWLDataProperty(), onts);
+        }
+        if (userObject.isOWLObjectProperty()) {
+            return EntitySearcher.getDomains(userObject.asOWLObjectProperty(), onts);
+        }
+        if (userObject.isObjectPropertyExpression()) {
+            return EntitySearcher.getDomains((OWLObjectPropertyExpression) userObject, onts);
+        }
+        return Collections.emptyList();
     }
 }
